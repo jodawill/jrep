c3c84490946b8abd50da25ab4e47082de74d2689
jodawill <jodawill1989@gmail.com>
2016-05-02 09:28:21 -0400

    Split source into multiple files

--- Makefile
+++ Makefile
@@ -1,5 +1,21 @@
 NAME = jrep
 CC=cc
+OBJECTS = src/jrep.o src/regex.o
+DEPS = src/jrep.h
 
-all:
-	$(CC) jrep.c -o jrep
+.PHONY: clean make install uninstall
+
+%.o: %.c $(DEPS)
+	$(CC) -c -o $@ $<
+
+$(NAME): $(OBJECTS)
+	$(CC) -o $@ $^ $(CFLAGS)
+
+install:
+	cp $(NAME) /usr/bin/$(NAME)
+
+uninstall:
+	rm /usr/bin/$(NAME)
+
+clean:
+	rm -f $(OBJECTS) $(NAME)
---
+++ src/jrep.h
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <strings.h>
+
+/* Specify GNU source for readline() function */
+#define _GNU_SOURCE
+
+/* Maximum number of states in a graph */
+#define MAX_EXPR   1024
+
+/* Define all of the possible states */
+#define T_CHAR        1
+#define T_EOL         2
+#define T_RANGE       3
+#define T_DOT         4
+
+/* An s_state struct stores a graph of states */
+struct s_state {
+ int type[MAX_EXPR];
+ int current;
+ int count;
+ char value[MAX_EXPR];
+ bool beg;
+ bool optional[MAX_EXPR];
+ bool kleene[MAX_EXPR];
+
+ struct {
+  int glb[MAX_EXPR];
+  int lub[MAX_EXPR];
+ } range;
+};
+
+int parse(char *expr, struct s_state *state);
+int does_char_match(char c, struct s_state *state, int i);
+int match_start(char *line, struct s_state *state, bool print, char **buffer);
+bool does_match(char *line, struct s_state *state, bool print, char **buffer);
